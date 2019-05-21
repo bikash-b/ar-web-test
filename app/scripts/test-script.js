677816f150e5875745a7a4a879bf6205632c5ea8
bikash-b <45403918+bikash-b@users.noreply.github.com>
2019-05-21 17:46:28 +0530

    Updated

--- app/scripts/test-script.js
+++ app/scripts/test-script.js
@@ -83,17 +83,17 @@
             })[0];
 
             //var orgLogo = getImageObject('-0.5 0.4 -1', 'app/assets/images/Roche.png');
-            var orgLogo = getImageObject('1 1 -0.2', 'app/assets/images/Roche.png');
+            var orgLogo = getImageObject('-0.5 0.4 -1', 'app/assets/images/Roche.png');
             var accessioningId = getInfoObject('1 1 0', '-90 0 0', 'Accessioning Id', latestProcessStepResult.accesssioningId);
             var assayType = getInfoObject('1 1 0.2', '-90 0 0', 'Assay type', order[0].assayType);
             var processStepName = getInfoObject('1 1 0.4', '-90 0 0', 'Process step name', latestProcessStepResult.processStepName);
-            //var status = getImageObject('1 -0.6 -1', `app/assets/images/${latestProcessStepResult.runStatus.toLowerCase()}.png`);
+            var status = getImageObject('1 -0.6 -1', `app/assets/images/${latestProcessStepResult.runStatus.toLowerCase()}.png`);
 
             markerElement.appendChild(orgLogo);
             markerElement.appendChild(accessioningId);
             markerElement.appendChild(assayType);
             markerElement.appendChild(processStepName);
-            //markerElement.appendChild(status);
+            markerElement.appendChild(status);
           }
         }, error => {});
       }, error => {});
